b94961ebde4be941a67177e68a28e0dde9ca426c
ttotti <prs12344@naver.com>
2019-08-23 03:33:27 +0900

    Auto_M ver_2.1

---
+++ Auto_M/Auto_M.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Auto_M
+{
+    partial class Auto_M
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Auto_M));
+            this.X_pos_label = new System.Windows.Forms.Label();
+            this.Y_pos_label = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.log_Label = new System.Windows.Forms.Label();
+            this.X_pos = new System.Windows.Forms.Label();
+            this.Y_pos = new System.Windows.Forms.Label();
+            this.num1_X_pos = new System.Windows.Forms.Label();
+            this.num1_Y_pos = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.num2_Y_pos = new System.Windows.Forms.Label();
+            this.num2_X_pos = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // X_pos_label
+            // 
+            this.X_pos_label.AutoSize = true;
+            this.X_pos_label.Location = new System.Drawing.Point(12, 51);
+            this.X_pos_label.Name = "X_pos_label";
+            this.X_pos_label.Size = new System.Drawing.Size(44, 12);
+            this.X_pos_label.TabIndex = 0;
+            this.X_pos_label.Text = "x좌표 :";
+            // 
+            // Y_pos_label
+            // 
+            this.Y_pos_label.AutoSize = true;
+            this.Y_pos_label.Location = new System.Drawing.Point(12, 78);
+            this.Y_pos_label.Name = "Y_pos_label";
+            this.Y_pos_label.Size = new System.Drawing.Size(44, 12);
+            this.Y_pos_label.TabIndex = 1;
+            this.Y_pos_label.Text = "y좌표 :";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "-현재 마우스 좌표-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "-1번 마우스좌표-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "x좌표 :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "y좌표 :";
+            // 
+            // log_Label
+            // 
+            this.log_Label.AutoSize = true;
+            this.log_Label.Location = new System.Drawing.Point(14, 304);
+            this.log_Label.Name = "log_Label";
+            this.log_Label.Size = new System.Drawing.Size(25, 12);
+            this.log_Label.TabIndex = 10;
+            this.log_Label.Text = "null";
+            // 
+            // X_pos
+            // 
+            this.X_pos.Location = new System.Drawing.Point(62, 51);
+            this.X_pos.Name = "X_pos";
+            this.X_pos.Size = new System.Drawing.Size(38, 15);
+            this.X_pos.TabIndex = 11;
+            this.X_pos.Text = "0";
+            // 
+            // Y_pos
+            // 
+            this.Y_pos.AutoSize = true;
+            this.Y_pos.Location = new System.Drawing.Point(62, 78);
+            this.Y_pos.Name = "Y_pos";
+            this.Y_pos.Size = new System.Drawing.Size(11, 12);
+            this.Y_pos.TabIndex = 12;
+            this.Y_pos.Text = "0";
+            // 
+            // num1_X_pos
+            // 
+            this.num1_X_pos.Location = new System.Drawing.Point(64, 140);
+            this.num1_X_pos.Name = "num1_X_pos";
+            this.num1_X_pos.Size = new System.Drawing.Size(100, 23);
+            this.num1_X_pos.TabIndex = 13;
+            this.num1_X_pos.Text = "0";
+            // 
+            // num1_Y_pos
+            // 
+            this.num1_Y_pos.AutoSize = true;
+            this.num1_Y_pos.Location = new System.Drawing.Point(64, 166);
+            this.num1_Y_pos.Name = "num1_Y_pos";
+            this.num1_Y_pos.Size = new System.Drawing.Size(11, 12);
+            this.num1_Y_pos.TabIndex = 14;
+            this.num1_Y_pos.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 408);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(191, 12);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "좌표 저장 - F3    좌표 초기화 - F2";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // num2_Y_pos
+            // 
+            this.num2_Y_pos.AutoSize = true;
+            this.num2_Y_pos.Location = new System.Drawing.Point(169, 166);
+            this.num2_Y_pos.Name = "num2_Y_pos";
+            this.num2_Y_pos.Size = new System.Drawing.Size(11, 12);
+            this.num2_Y_pos.TabIndex = 20;
+            this.num2_Y_pos.Text = "0";
+            // 
+            // num2_X_pos
+            // 
+            this.num2_X_pos.Location = new System.Drawing.Point(169, 140);
+            this.num2_X_pos.Name = "num2_X_pos";
+            this.num2_X_pos.Size = new System.Drawing.Size(100, 23);
+            this.num2_X_pos.TabIndex = 19;
+            this.num2_X_pos.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(119, 166);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 12);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "y좌표 :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(119, 140);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 12);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "x좌표 :";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(117, 114);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(99, 12);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "-2번 마우스좌표-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 429);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(164, 12);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "오토마우스 실행 및 종료 - F4";
+            // 
+            // Auto_M
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.num2_Y_pos);
+            this.Controls.Add(this.num2_X_pos);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.num1_Y_pos);
+            this.Controls.Add(this.num1_X_pos);
+            this.Controls.Add(this.Y_pos);
+            this.Controls.Add(this.X_pos);
+            this.Controls.Add(this.log_Label);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Y_pos_label);
+            this.Controls.Add(this.X_pos_label);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Auto_M";
+            this.Text = "이것이 오토인지 뭔지 그런거냐?";
+            this.Load += new System.EventHandler(this.Auto_M_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label X_pos_label;
+        private System.Windows.Forms.Label Y_pos_label;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label log_Label;
+        private System.Windows.Forms.Label X_pos;
+        private System.Windows.Forms.Label Y_pos;
+        private System.Windows.Forms.Label num1_X_pos;
+        private System.Windows.Forms.Label num1_Y_pos;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label num2_Y_pos;
+        private System.Windows.Forms.Label num2_X_pos;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Auto_M/Auto_M.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// [DllImport]를 사용하기 위해 추가한다
+using System.Runtime.InteropServices;
+
+namespace Auto_M
+{
+    public partial class Auto_M : Form
+    {
+        // winapi 함수를 쓰기 위해 선언함
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(ref Point point);
+
+        [DllImport("user32.dll")]
+        public static extern void SetCursorPos(Int32 x, Int32 y);
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
+
+        // 핫키를 사용하기 위한 winapi 함수
+        // 핫키를 등록한다
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+        // 핫키를 해제한다
+        // 해당 핸들의 아이디에 있는 핫키를 해제한다
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+
+
+        // c# 에서는 C/C++에서 사용하는 #define 을 못쓴다
+        // 대신 아래와 같이 상수를 정의한다
+        const int WM_HOTKEY = 0x0312;
+
+        public const int F2 = 113;
+        public const int F3 = 114;
+        public const int F4 = 115;
+
+        public const uint LBUTTONDOWN = 0x0002;
+        public const uint LBUTTONUP = 0x0004;
+
+        // 마우스 좌표
+        Point main_point;
+        Point num1_point;
+        Point num2_point;
+
+        // 오토마우스가 실행되고 있는가
+        bool isStart = false;
+
+        // 윈도우 프로시저 콜백 함수
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            // 핫키가 눌러지면 312 정수 메시지를 받게됨 // WM_HOTKEY로 312를 상수정의
+            if(m.Msg == WM_HOTKEY)
+            {
+                // 눌러진 키의 ID가 0이면
+                if (m.WParam == (IntPtr)0x0)
+                {
+                    if(!isStart)
+                    {
+                        isStart = true;
+
+                        log_Label.Text = "오토마우스가 실행되었습니다!";
+
+                        timer2.Start();
+                    }
+                    else
+                    {
+                        isStart = false;
+
+                        log_Label.Text = "오토마우스가 종료되었습니다!";
+
+                        timer2.Stop();
+                    }
+                }
+                // 눌러진 키의 ID가 1이면
+                else if (m.WParam == (IntPtr)0x1)
+                {
+                    if (num1_point.X == 0 && num1_point.Y == 0)
+                    {
+                        num1_point = main_point;
+                        num1_X_pos.Text = num1_point.X.ToString();
+                        num1_Y_pos.Text = num1_point.Y.ToString();
+
+                        log_Label.Text = "첫 번째 좌표가 저장되었습니다!";
+                    }
+                    else if (num2_point.X == 0 && num2_point.Y == 0)
+                    {
+                        num2_point = main_point;
+                        num2_X_pos.Text = num2_point.X.ToString();
+                        num2_Y_pos.Text = num2_point.Y.ToString();
+
+                        log_Label.Text = "두 번째 좌표가 저장되었습니다!";
+                    }
+                }
+                // 눌러진 키의 ID가 2이면
+                else if (m.WParam == (IntPtr)0x2)
+                {
+                    num1_point.X = 0;
+                    num1_point.Y = 0;
+
+                    num1_X_pos.Text = num1_point.X.ToString();
+                    num1_Y_pos.Text = num1_point.Y.ToString();
+
+                    num2_point.X = 0;
+                    num2_point.Y = 0;
+
+                    num2_X_pos.Text = num2_point.X.ToString();
+                    num2_Y_pos.Text = num2_point.Y.ToString();
+
+                    log_Label.Text = "모든 좌표가 초기화되었습니다!";
+                }
+            }
+        }
+
+        public Auto_M()
+        {
+            InitializeComponent();
+
+            main_point.X = 0;
+            main_point.Y = 0;
+
+            num1_point.X = 0;
+            num1_point.Y = 0;
+
+            num2_point.X = 0;
+            num2_point.Y = 0;
+        }
+
+        private void Auto_M_Load(object sender, EventArgs e)
+        {
+            // 핫키를 등록
+            // RegisterHotkey(핫키를 입력받을 핸들, id 값, modifer 라고 불리우는 키값(ctrl, alt, shift, winkey 등 조합키, 어떤 키를 누를지)
+            // 조합키 - 0x0: 조합키안씀  |  0x1: alt  | 0x2: ctrl  |  0x3: shift
+            RegisterHotKey(this.Handle, 0, 0x0, (int)Keys.F4);
+            RegisterHotKey(this.Handle, 1, 0x0, (int)Keys.F3);
+            RegisterHotKey(this.Handle, 2, 0x0, (int)Keys.F2);
+            timer1_Tick(sender, e);
+            timer1.Start();
+        }
+
+        // 핫키코드로 바꾸기 전 코드 // 키보드 이벤트 함수를 만들어서 메인폼 keyDown에 추가시킨다
+        //private void SaveKeyDown_KeyDown(object sender, KeyEventArgs e)
+        //{
+        //    log_Label.Text = "키보드 이벤트 발생!";
+
+        //    if (e.KeyValue == F3)
+        //    {
+        //        if (num1_point.X == 0 && num1_point.Y == 0)
+        //        {
+        //            num1_point = main_point;
+        //            num1_X_pos.Text = num1_point.X.ToString();
+        //            num1_Y_pos.Text = num1_point.Y.ToString();
+
+        //            log_Label.Text = "첫 번째 좌표가 저장되었습니다!";
+        //        }
+        //        else if (num2_point.X == 0 && num2_point.Y == 0)
+        //        {
+        //            num2_point = main_point;
+        //            num2_X_pos.Text = num2_point.X.ToString();
+        //            num2_Y_pos.Text = num2_point.Y.ToString();
+
+        //            log_Label.Text = "두 번째 좌표가 저장되었습니다!";
+        //        }
+        //    }
+        //    else if (e.KeyValue == F2)
+        //    {
+        //        num1_point.X = 0;
+        //        num1_point.Y = 0;
+
+        //        num1_X_pos.Text = num1_point.X.ToString();
+        //        num1_Y_pos.Text = num1_point.Y.ToString();
+
+        //        num2_point.X = 0;
+        //        num2_point.Y = 0;
+
+        //        num2_X_pos.Text = num2_point.X.ToString();
+        //        num2_Y_pos.Text = num2_point.Y.ToString();
+
+        //        log_Label.Text = "모든 좌표가 초기화되었습니다!";
+        //    }
+        //    else if (e.KeyValue == F4)
+        //    {
+        //        if (!isStart)
+        //        {
+        //            log_Label.Text = "오토마우스가 실행되었습니다!";
+
+        //            isStart = true;
+        //            // 타이머 간격
+        //            timer2.Interval = 3000;
+        //            //timer2_Tick += new EventHandler(timer2_Tick);
+        //            timer2.Start();
+        //        }
+        //        else
+        //        {
+        //            log_Label.Text = "오토마우스가 종료되었습니다!";
+        //            isStart = false;
+        //            timer2.Stop();
+        //        }
+        //    }
+        //}
+
+        private void timer1_Tick(object sender, EventArgs e) // 타이머
+        {
+            GetCursorPos(ref main_point);
+            X_pos.Text = main_point.X.ToString();
+            Y_pos.Text = main_point.Y.ToString();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e) // 타이머2
+        {
+            // 타이머 일시정지 // 1000밀리초 = 1초
+            System.Threading.Thread.Sleep(1000);
+
+            SetCursorPos(num1_point.X, num1_point.Y);
+            mouse_event(LBUTTONDOWN, 0, 0, 0, 0);
+            mouse_event(LBUTTONUP, 0, 0, 0, 0);
+
+            // 타이머 일시정지
+            System.Threading.Thread.Sleep(1000);
+
+            SetCursorPos(num2_point.X, num2_point.Y);
+            mouse_event(LBUTTONDOWN, 0, 0, 0, 0);
+            mouse_event(LBUTTONUP, 0, 0, 0, 0);
+        }
+    }
+}
+
+// 핫키가 등록되는 즉시 핫키에 지정된 핸들외에 다른 프로그램들은 해당 키의 입력을 전달받지 못한다
+// 익스플로러에서 F5는 새로고침이지만 F5를 핫키로 등록했다면
+// F5를 눌러도 새로고침 이라는 기능은 실행되지 않습니다
+// 상황에 따라서 의도적으로 이용하거나 신경쓰지 않아도 되겠지만
+// 그렇지 않다면 다른 프로그램의 단축키를 피해서 설정하거나 키입력을 다시 돌려주는 등의 해결방안을 신경써야합니다
+
+// 도움이 된 사이트
+// https://11cc.tistory.com/10
+// https://www.hobbiez.ml/archives/174
